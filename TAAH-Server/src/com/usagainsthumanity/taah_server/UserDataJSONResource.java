f34b22f47dd30f70592576024c0a50e3f76a3d98
Stewart Boling <sjbkcc@mst.edu>
2013-02-13 14:41:10 -0600

    Resolves issue #14

--- TAAH-Server/src/com/usagainsthumanity/taah_server/UserDataJSONResource.java
+++ TAAH-Server/src/com/usagainsthumanity/taah_server/UserDataJSONResource.java
@@ -1,6 +1,10 @@
 package com.usagainsthumanity.taah_server;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -8,7 +12,10 @@ import org.restlet.ext.json.JsonRepresentation;
 import org.restlet.representation.Representation;
 import org.restlet.resource.Get;
 import org.restlet.resource.Put;
+import org.restlet.resource.ResourceException;
 import org.restlet.resource.ServerResource;
+
+import com.usagainsthumanity.taah_model.UserInfo;
 
 public class UserDataJSONResource extends ServerResource {
 	@Get("json")
@@ -16,12 +23,23 @@ public class UserDataJSONResource extends ServerResource {
 		try{
 			//TODO All of the things
 			String userID = (String)this.getRequestAttributes().get("userId");
-			
-			JSONObject json = new JSONObject();
-			json.put("name", userID); 
-
-			JsonRepresentation jsonRep = new JsonRepresentation(json);
-			return jsonRep.getText();
+			EntityManager em = EMFService.get().createEntityManager();
+			Query q = em.createQuery("select u from UserInfo u where e.userID = :userID");
+			q.setParameter("userID", userID);
+			List<UserInfo> users = q.getResultList();
+			if(users.size() == 1){
+				JSONObject json = new JSONObject();
+				json.put("ID", users.get(0).getUserID());
+				json.put("Name", users.get(0).getFirstName() + " " + users.get(0).getLastName());
+				json.put("Email", users.get(0).getEmail());
+				json.put("Profile Picture", users.get(0).getProfilePicture());				
+				JsonRepresentation jsonRep = new JsonRepresentation(json);
+				return jsonRep.getText();
+			}else if(users.size() > 1){
+				throw new ResourceException(406);
+			}else{
+				throw new ResourceException(404);
+			}
 		} catch (JSONException e) {
 			e.printStackTrace();
 		} catch (IOException e){
