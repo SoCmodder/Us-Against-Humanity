e6fe8340776cf66daf8b1a1521249e854a7b6f06
Courier00 <Courier00@hotmail.com>
2013-02-13 15:47:58 -0600

    Resolved issue #16

--- TAAH-Server/src/com/usagainsthumanity/taah_server/WhiteCardJSONResource.java
+++ TAAH-Server/src/com/usagainsthumanity/taah_server/WhiteCardJSONResource.java
@@ -1,12 +1,19 @@
 package com.usagainsthumanity.taah_server;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.restlet.ext.json.JsonRepresentation;
 import org.restlet.resource.Get;
+import org.restlet.resource.ResourceException;
 import org.restlet.resource.ServerResource;
+
+import com.usagainsthumanity.taah_model.CardWhite;
 
 public class WhiteCardJSONResource extends ServerResource {
 	
@@ -14,12 +21,21 @@ public class WhiteCardJSONResource extends ServerResource {
 	public String handleGet() {
 		try{
 			//TODO All of the things
-			String cardId = getQuery().getValues("cardId");
-			JSONObject json = new JSONObject();
-			json.put("Text", "Sexual Tension"); 
-
-			JsonRepresentation jsonRep = new JsonRepresentation(json);
-			return jsonRep.getText();
+			String cardID = getQuery().getValues("cardID");
+			EntityManager em = EMFService.get().createEntityManager();
+			Query q = em.createQuery("select c from CardWhite c where e.cardID = :cardID");
+			q.setParameter("cardID", cardID);
+			List<CardWhite> cards = q.getResultList();
+			if (cards.size() == 1){
+				JSONObject json = new JSONObject();
+				json.put("Text", cards.get(0).getDescription());
+				JsonRepresentation jsonRep = new JsonRepresentation(json);
+				return jsonRep.getText();
+			}else if (cards.size() > 1){
+				throw new ResourceException(406);
+			} else{
+				throw new ResourceException(404);
+			}			
 		} catch (JSONException e) {
 			e.printStackTrace();
 		} catch (IOException e){
