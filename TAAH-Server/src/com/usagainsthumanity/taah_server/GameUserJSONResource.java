628f5d8d37112461eae1075129e34dfc2796c2fe
ThredNThrash <ThredNThrash@gmail.com>
2013-02-21 05:39:42 -0600

    Resolves Get part of Issue #13.

--- TAAH-Server/src/com/usagainsthumanity/taah_server/GameUserJSONResource.java
+++ TAAH-Server/src/com/usagainsthumanity/taah_server/GameUserJSONResource.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
 import org.json.JSONObject;
 import org.restlet.ext.json.JsonRepresentation;
 import org.restlet.representation.Representation;
@@ -12,17 +15,24 @@ import org.restlet.resource.Put;
 import org.restlet.resource.ResourceException;
 import org.restlet.resource.ServerResource;
 
+import com.usagainsthumanity.taah_model.GameUsers;
+
 public class GameUserJSONResource extends ServerResource {
 
 	@Get("json")
 	public String getUsers(){
-		Integer gameID = Integer.parseInt((String)this.getRequestAttributes().get("gameId"));
+		Integer gameID = Integer.parseInt((String)this.getRequestAttributes().get("gameID"));
 		try{
+			// Need to check if they game doesn't exist and return it still
 			JSONObject json = new JSONObject();
+			EntityManager em = EMFService.get().createEntityManager();
+			Query q = em.createQuery("select g from GameUsers g where g.gameID = :gameID");
+			q.setParameter("gameID", gameID);
+			List<GameUsers> gameUsers = q.getResultList();
 			List<Integer> Users = new LinkedList<Integer>();
-			Users.add(1234);
-			Users.add(5678);
-			Users.add(9101112);
+			for(GameUsers gameUserList : gameUsers) {
+				Users.add(gameUserList.getUserID());
+			}
 			json.put("Users", Users);
 			JsonRepresentation jsonRep = new JsonRepresentation(json);
 			return jsonRep.getText();
@@ -34,6 +44,7 @@ public class GameUserJSONResource extends ServerResource {
 
 	@Put
 	public void putUser(Representation entity) throws IOException{
+		// Didn't implement this because I'm not sure how to add users to the game
 		Integer gameID = Integer.parseInt((String)this.getRequestAttributes().get("gameId"));
 		try{
 			System.out.println(entity.getText());
