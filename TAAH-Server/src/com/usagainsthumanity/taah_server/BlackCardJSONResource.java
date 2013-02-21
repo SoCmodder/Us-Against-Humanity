403ff82cf01a4405663464c76d36da193167f8b9
ThredNThrash <ThredNThrash@gmail.com>
2013-02-21 04:54:17 -0600

    Resolves issue #7

--- TAAH-Server/src/com/usagainsthumanity/taah_server/BlackCardJSONResource.java
+++ TAAH-Server/src/com/usagainsthumanity/taah_server/BlackCardJSONResource.java
@@ -2,11 +2,16 @@ package com.usagainsthumanity.taah_server;
 
 import java.io.IOException;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.restlet.ext.json.JsonRepresentation;
 import org.restlet.resource.Get;
 import org.restlet.resource.ServerResource;
+
+import com.usagainsthumanity.taah_model.CardBlack;
 
 public class BlackCardJSONResource extends ServerResource {
 	
@@ -14,10 +19,13 @@ public class BlackCardJSONResource extends ServerResource {
 	public String handleGet() {
 		try{
 			//TODO All of the things
-			String cardId = getQuery().getValues("cardId");
+			String cardId = getQuery().getValues("cardID");
+			EntityManager em = EMFService.get().createEntityManager();
+			Query q = em.createQuery("select c from CardBlack c where c.cardID = :cardID");
+			q.setParameter("cardID", cardId);
+			CardBlack theCard = (CardBlack)q.getSingleResult();
 			JSONObject json = new JSONObject();
-			json.put("Text", "In a world ravaged by ________ our only solice rests in _______ (Pick 2)"); 
-
+			json.put("Text", theCard.getCardText());
 			JsonRepresentation jsonRep = new JsonRepresentation(json);
 			return jsonRep.getText();
 		} catch (JSONException e) {
