678d31f0087924cb2f25a0bf2bb7312ce821ba8d
ThredNThrash <ThredNThrash@gmail.com>
2013-02-26 17:59:19 -0600

    Added score and changed Integers to Longs

--- TAAH-Server/src/com/usagainsthumanity/taah_model/GameUsers.java
+++ TAAH-Server/src/com/usagainsthumanity/taah_model/GameUsers.java
@@ -7,15 +7,18 @@ import javax.persistence.Id;
 public class GameUsers {
 
 	@Id
-	private Integer gameID;
-	private Integer userID;
+	private Long gameID;
+	private Long userID;
+	private Long score;
 	/**
 	 * @param gameID
 	 * @param userID
+	 * @param score
 	 */
-	public GameUsers(Integer gameID, Integer userID) {
+	public GameUsers(Long gameID, Long userID, Long score) {
 		this.gameID = gameID;
 		this.userID = userID;
+		this.score = score;
 	}
 
     public GameUsers() {
@@ -24,28 +27,38 @@ public class GameUsers {
     /**
 	 * @return the gameID
 	 */
-	public Integer getGameID() {
+	public Long getGameID() {
 		return gameID;
 	}
 	
 	/**
 	 * @param gameID the gameID to set
 	 */
-	public void setGameID(Integer gameID) {
+	public void setGameID(Long gameID) {
 		this.gameID = gameID;
 	}
 	/**
 	 * @return the userID
 	 */
-	public Integer getUserID() {
+	public Long getUserID() {
 		return userID;
 	}
 	/**
 	 * @param userID the userID to set
 	 */
-	public void setUserID(Integer userID) {
+	public void setUserID(Long userID) {
 		this.userID = userID;
 	}
-	
-	
+	/**
+	 * @return the score
+	 */
+	public Long getScore() {
+		return score;
+	}
+	/**
+	 * @param score the score to set
+	 */
+	public void setScore(Long score) {
+		this.score = score;
+	}
 }
