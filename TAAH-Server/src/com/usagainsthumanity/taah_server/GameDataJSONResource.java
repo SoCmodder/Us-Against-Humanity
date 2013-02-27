08638922ce3aaa8222a2797b0447cd2aaffbf9ba
ThredNThrash <ThredNThrash@gmail.com>
2013-02-26 18:12:26 -0600

    Resovles Most of #9 (Don't know what HasPlayed is about, and not sure how to do the delete part)

--- TAAH-Server/src/com/usagainsthumanity/taah_server/GameDataJSONResource.java
+++ TAAH-Server/src/com/usagainsthumanity/taah_server/GameDataJSONResource.java
@@ -3,6 +3,9 @@ package com.usagainsthumanity.taah_server;
 import java.util.LinkedList;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
 import org.json.JSONObject;
 import org.restlet.ext.json.JsonRepresentation;
 import org.restlet.resource.Delete;
@@ -10,28 +13,40 @@ import org.restlet.resource.Get;
 import org.restlet.resource.ResourceException;
 import org.restlet.resource.ServerResource;
 
+import com.usagainsthumanity.taah_model.Game;
+import com.usagainsthumanity.taah_model.GameUsers;
+
 public class GameDataJSONResource extends ServerResource {
 	
 	@Get("json")
 	public String getStats(){
 		//TODO Actual lookup
 		try{
-			String gameID = (String)this.getRequestAttributes().get("gameId");
+			String gameID = (String)this.getRequestAttributes().get("gameID");
+			EntityManager em = EMFService.get().createEntityManager();
+			// Get all the gameUser objects with the gameID
+			Query q = em.createQuery("select g from GameUsers g where g.gameID = :gameID");
+			q.setParameter("gameID", gameID);
 			JSONObject json = new JSONObject();
-			List<Integer> Scores = new LinkedList<Integer>();
-			Scores.add(1);
-			Scores.add(0);
-			Scores.add(0);
-			List<Integer> IDS = new LinkedList<Integer>();
-			IDS.add(6969);
-			IDS.add(1337);
-			IDS.add(80085);
-			List<Integer> HasPlayed = new LinkedList<Integer>();
-			HasPlayed.add(1);
+			List<GameUsers> gameUsers = q.getResultList();
+			List<Long> Scores = new LinkedList<Long>();
+			List<Long> IDS = new LinkedList<Long>();
+			for(GameUsers resultList : gameUsers) {
+				// Loops through all gameUsers and gets their userIDs and scores
+				IDS.add(resultList.getUserID());
+				Scores.add(resultList.getScore());
+			}
+			// Not sure what this is supposed to be
+			List<Long> HasPlayed = new LinkedList<Long>();
+			//HasPlayed.add(1);
 			json.put("Scores", Scores);
 			json.put("IDs", IDS);
-			json.put("CardsToWin", 99);
-			json.put("PlayerTurn", 1);
+			q = em.createQuery("select g from Game g where g.gameID = :gameID");
+			q.setParameter("gameID", gameID);
+			List<Game> game = new LinkedList<Game>();
+			game = q.getResultList();
+			json.put("CardsToWin", game.get(0).getPointsToWin());
+			json.put("PlayerTurn", game.get(0).getUserTurn());
 			json.put("HasPlayed", HasPlayed);
 			JsonRepresentation jsonRep = new JsonRepresentation(json);
 			return jsonRep.getText();
