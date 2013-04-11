a30d580b4cc9f0e45706d039c7fecea608fc2a2b
Stewart Boling <sjbkcc@mst.edu>
2013-04-11 13:48:40 -0500

    Fixed compile error

---
+++ App/src/com/cs/usagainsthumanity/ViewGameHistoryFragment.java
@@ -0,0 +1,13 @@
+package com.cs.usagainsthumanity;
+
+import com.actionbarsherlock.app.SherlockFragment;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Stewart
+ * Date: 4/11/13
+ * Time: 1:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ViewGameHistoryFragment extends SherlockFragment{
+}
