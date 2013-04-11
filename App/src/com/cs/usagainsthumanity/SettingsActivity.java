718ed149a21bcbfe856ed01fdaf975154cbfaa00
Stewart Boling <sjbkcc@mst.edu>
2013-04-11 13:57:27 -0500

    Added moar activities

---
+++ App/src/com/cs/usagainsthumanity/SettingsActivity.java
@@ -0,0 +1,13 @@
+package com.cs.usagainsthumanity;
+
+import com.actionbarsherlock.app.SherlockPreferenceActivity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Stewart
+ * Date: 4/11/13
+ * Time: 1:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SettingsActivity extends SherlockPreferenceActivity {
+}
---
+++ App/src/com/cs/usagainsthumanity/UserInfoActivity.java
@@ -0,0 +1,13 @@
+package com.cs.usagainsthumanity;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Stewart
+ * Date: 4/11/13
+ * Time: 1:54 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserInfoActivity extends SherlockActivity {
+}
